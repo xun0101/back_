a4c3ad3ba70084f19d10761868d078f4297a3b4c
xun <dabby6521@gmail.com>
2022-02-09 16:29:07 +0800

    0209

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import usersRouter from './routes/users.js'
+import productsRouter from './routes/products.js'
 
 mongoose.connect(process.env.DB_URL, () => {
   console.log('MongoDB Connected')
@@ -28,6 +29,7 @@ app.use((_, req, res, next) => {
 })
 
 app.use('/users', usersRouter)
+app.use('/products', productsRouter)
 
 app.all('*', (req, res) => {
   console.log(req.body)
