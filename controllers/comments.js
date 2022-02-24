ac69faac67b43c78551a858709e1ba8700c3c785
xun <dabby6521@gmail.com>
2022-02-24 16:27:14 +0800

    0224

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,7 +2,7 @@ import comments from '../models/comments.js'
 
 export const addcomment = async (req, res) => {
   try {
-    const result = await comments.create({ ...req.body, user: req.user.id })
+    const result = await comments.create({ ...req.body, user: req.user })
     console.log(result)
     res.status(200).send({ success: true, message: '', result })
   } catch (error) {
@@ -22,5 +22,28 @@ export const getAllcomments = async (req, res) => {
     res.status(200).send({ success: true, message: '', result })
   } catch (error) {
     res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+export const delcomments = async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const result = await comments.findByIdAndDelete(req.params.id)
+    if (result === null) {
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(200)
+      res.send({ success: true, message: '' })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      console.log(error)
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(500)
+      res.send({ success: false, message: '伺服器發生錯誤' })
+    }
   }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
+import admin from '../middleware/admin.js'
+
 import {
   addcomment,
-  getAllcomments
+  getAllcomments,
+  delcomments
 } from '../controllers/comments.js'
 
 const router = express.Router()
 
 router.post('/', auth, addcomment)
 router.get('/all', getAllcomments)
+router.delete('/dels/:id', auth, admin, delcomments)
 
 export default router
