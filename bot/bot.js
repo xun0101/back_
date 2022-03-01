5fd486ee76d6da405190b798e33b44def09cbf6e
xun <dabby6521@gmail.com>
2022-03-01 16:33:59 +0800

    0301

--- bot/bot.js
+++ bot/bot.js
@@ -8,22 +8,4 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN_BOT
 })
 
-// bot.on('message', event => {
-//   console.log(event)
-
-//   if (event.type === 'message') {
-//     try {
-//       if (event.message.type === 'text') {
-//         event.reply('感謝您的等候，當有空位時我們會盡快通知您').then(
-//           setTimeout(function () {
-//             bot.push(event.source.userId, '感謝您的耐心等候，現在已有空位，歡迎您的蒞臨')
-//           }, 60000)
-//         )
-//       }
-//     } catch (error) {
-//       console.log()
-//     }
-//   }
-// })
-
 export default bot
